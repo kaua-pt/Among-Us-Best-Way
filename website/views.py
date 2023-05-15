67f9b421d9d7d80f119e62f9e509a4a14f83474b
Kauã Vinícius <81399319+kaua-pt@users.noreply.github.com>
2023-05-14 23:33:30 -0300

    Finalizando dia de hoje

--- website/views.py
+++ website/views.py
@@ -7,3 +7,8 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("home.html", tasks=tasks)
+
+
+@views.route('/map')
+def map():
+    return render_template("map.html")
