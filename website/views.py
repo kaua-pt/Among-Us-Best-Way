680a7f6dd1df30c0640e1a130f96641abe18aad4
Kauã Vinícius <81399319+kaua-pt@users.noreply.github.com>
2023-07-28 22:24:27 -0300

    Iniciando novo front

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request as req
 from .utils import tasks
 
 views = Blueprint('views', __name__)
@@ -9,6 +9,14 @@ def home():
     return render_template("home.html", tasks=tasks)
 
 
+@views.route('/select')
+def select():
+    return render_template("select.html")
+
+
 @views.route('/map')
 def map():
+    for element in tasks:
+        if (req):
+            print(req.form['id'])
     return render_template("map.html")
